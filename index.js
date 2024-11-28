7b38c84fe82f0c28999f8c3235ae2da51c999f94
zxcvbnm7854623 <3083810542@qq.com>
2024-11-29 00:02:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow } from "https://cdn.jsdelivr.net/npm/date-fns@2.28.0/index.js";
 
 const date = "1996-09-13 10:00:00";
 document.body.textContent = `${formatDistanceToNow(new Date(date))} ago`;
